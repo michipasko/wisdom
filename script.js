9248949c2b7433843d27273df07f973feb5367d6
michipasko <57601557+michipasko@users.noreply.github.com>
2022-06-01 22:51:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ const wisdomOfTheWorld = [
   "Wenn mein Hund sagt, dass er kacken muss, dann lass ich ihn.",
   "Der Coach sagt: Vollgas!",
   "Red dir niemals ein, dass du Schuld bist. Es ist ganz sicher jemand anderes.",
-  "Wenn man ganz genau hinsieht, kann man mit etwas Glück mehr erkennen."
-  "Runde Bälle haben keine Ecken."
+  "Wenn man ganz genau hinsieht, kann man mit etwas Glück mehr erkennen.",
+  "Runde Bälle haben keine Ecken.",
+  "Wenn dir das Leben eine Zitrone gibt, dann sag: 'Behalt deinen Scheiß'.
 ];
 
 const btnWisdom = document.getElementById("btn-wisedom");
